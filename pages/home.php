e54714bbe99d054de6ca7a4d4d8e87ec365f39d1
Md. Mamun Ar Rashid <mamunarrashid.cse@gmail.com>
2022-02-21 00:26:03 +0600

    assignment

---
+++ pages/home.php
@@ -0,0 +1,86 @@
+<?php include 'pages/includes/header.php'; ?>
+<section class="py-5 bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-4">
+                                <img src="assets/img/mamun2.jpg" class="img-fluid overflow-hidden w-100" alt="">
+                                <h2>T-Shart</h2>
+
+                                <p>Description</p>
+                                <a href="#" class="align-self-end">View</a>
+                            </div>
+                            <div class="col-4">
+                                <img src="assets/img/mamun2.jpg" class="img-fluid overflow-hidden w-100" alt="">
+                                <h2>T-Shart</h2>
+
+                                <p>Description</p>
+                                <a href="#" class="align-self-end">View</a>
+                            </div>
+                            <div class="col-4">
+                                <img src="assets/img/mamun2.jpg" class="img-fluid overflow-hidden w-100" alt="">
+                                <h2>T-Shart</h2>
+
+                                <p>Description</p>
+                                <a href="#" class="align-self-end">View</a>
+                            </div>
+                            <div class="col-3">
+                                <img src="assets/img/mamun2.jpg" class="img-fluid overflow-hidden w-100" alt="">
+                                <h2>T-Shart</h2>
+
+                                <p>Description</p>
+                                <a href="#" class="align-self-end">View</a>
+                            </div>
+                            <div class="col-3">
+                                <img src="assets/img/mamun2.jpg" class="img-fluid overflow-hidden w-100" alt="">
+                                <h2>T-Shart</h2>
+
+                                <p>Description</p>
+                                <a href="#" class="align-self-end">View</a>
+                            </div>
+                            <div class="col-3">
+                                <img src="assets/img/mamun2.jpg" class="img-fluid overflow-hidden w-100" alt="">
+                                <h2>T-Shart</h2>
+
+                                <p>Description</p>
+                                <a href="#" class="align-self-end">View</a>
+                            </div>
+                            <div class="col-3">
+                                <img src="assets/img/mamun2.jpg" class="img-fluid overflow-hidden w-100" alt="">
+                                <h2>T-Shart</h2>
+
+                                <p>Description</p>
+                                <a href="#" class="align-self-end">View</a>
+                            </div>
+                            <div class="col-4">
+                                <img src="assets/img/mamun2.jpg" class="img-fluid overflow-hidden w-100" style="height: 160px;" alt="">
+                                <h2>T-Shart</h2>
+
+                                <p>Description</p>
+                                <a href="#" class="align-self-end">View</a>
+                            </div>
+                            <div class="col-4">
+                                <img src="assets/img/mamun2.jpg" class="img-fluid overflow-hidden w-100" alt="">
+                                <h2>T-Shart</h2>
+
+                                <p>Description</p>
+                                <a href="#" class="align-self-end">View</a>
+                            </div>
+                            <div class="col-4">
+                                <img src="assets/img/mamun3.jpg" class="img-fluid overflow-hidden w-100" alt="">
+                                <h2>T-Shart</h2>
+
+                                <p>Description</p>
+                                <a href="#" class="align-self-end">View</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php include 'pages/includes/footer.php'; ?>
